7828477e81878580b092a57b31eede5433dc4148
hugocp9 <hv8laptop@gmail.com>
2021-09-27 18:02:32 +0200

    create home page component and base footer and header

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name : 'HomePage',
+  data : function () {
+    return {
+      msg : 'this is the home page'
+    }
+  },
+  components : {
+
+  }
+}
+</script>
+
+<style>
+
+
+</style>
